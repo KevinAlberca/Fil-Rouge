89bae0568984dbbe3f40805333d1889afa4af88d
Kevin ALBERCA <kevin.alberca@supinternet.fr>
2015-12-11 10:52:13 +0100

    Repository creee

---
+++ src/AK/BlogBundle/Entity/PostRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AwH
+ * Date: 11/12/15
+ * Time: 10:50
+ */
+
+namespace AK\BlogBundle\Entity;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class PostRepository extends EntityRepository
+{
+
+}
